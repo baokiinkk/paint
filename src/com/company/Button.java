5323a03a3461fcaf5d89414a6039eb3f05eed0db
Tran Nguyen Chi Nhan <39859635+tncn1122@users.noreply.github.com>
2020-04-18 19:40:26 +0700

    Complete drawing Line button. Fixed bug when drawing multiple lines.

---
+++ src/com/company/Button.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public enum Button {
+    MOUSE,
+    CLEAR,
+    LINE,
+    PENCIL
+
+}
--- src/com/company/paint.java
+++ src/com/company/paint.java
@@ -3,17 +3,21 @@ package com.company;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javafx.util.Pair;
+import java.util.ArrayList;
+import com.company.Button;
 
 public class paint  extends JFrame {
-    public int mX = -1;
-    public int mY = -1;
-    public int spacing = 1;
-    public int rectSize = 5;
-    int choose=1;
-    public int xStart=-1;
-    public int yStart=-1;
-    public boolean[][] drawingBoard = new boolean[220][155]; //150x217
-
+    private int mX = -1;
+    private int mY = -1;
+    private int spacing = 1;
+    private int rectSize = 5;
+    private Button choose = Button.MOUSE;
+    private int xStart=-1;
+    private int yStart=-1;
+    private boolean firstClick = false;
+    private boolean[][] drawingBoard = new boolean[220][155]; //150x217
+    private ArrayList<Pair<Integer, Integer>> previousPoint = new ArrayList <Pair <Integer, Integer> > ();
 
     private JPanel activity_main;
     private JPanel leftPanel;
@@ -76,22 +80,41 @@ public class paint  extends JFrame {
     private void setPoint(int cordX, int cordY)
     {
         drawingBoard[cordX/(rectSize)][cordY/(rectSize)] = true;
+
+    }
+    private boolean isSettedPoint(int cordX, int cordY)
+    {
+        return drawingBoard[cordX/(rectSize)][cordY/(rectSize)];
     }
 
     private void clearPoint(int cordX, int cordY)
     {
-        drawingBoard[cordX/(rectSize)][cordY/(rectSize)] = true;
+        drawingBoard[cordX/(rectSize)][cordY/(rectSize)] = false;
     }
     private void MidpointLine(int x1, int y1, int x2, int y2)
     {
         float x=x1,y=y1,temp=(Math.abs(x2-x1)>=Math.abs(y2-y1))?Math.abs(x2-x1):Math.abs(y2-y1);
-        setPoint((int)(x+0.5),(int)(y+0.5));
+        int tx = (int)(x+0.5);
+        int ty = (int)(y+0.5);
+        if (!isSettedPoint(tx, ty))
+        {
+            setPoint(tx, ty);
+            previousPoint.add(new Pair <Integer, Integer> (tx, ty));
+        }
+
         for(int i=0;i<temp;i++)
         {
-            x+=(x2-x1)/temp;
-            y+=(y2-y1)/temp;
-            setPoint((int)(x+0.5),(int)(y+0.5));
+            x += (x2-x1)/temp;
+            y += (y2-y1)/temp;
+            tx = (int)(x+0.5);
+            ty = (int)(y+0.5);
+            if (!isSettedPoint(tx, ty))
+            {
+                setPoint(tx, ty);
+                previousPoint.add(new Pair <Integer, Integer> (tx, ty));
+            }
         }
+        System.out.println(previousPoint.size());
     }
 
     private void clearAll()
@@ -105,12 +128,21 @@ public class paint  extends JFrame {
         }
     }
 
+    private void clearPrevious()
+    {
+        for (Pair<Integer, Integer> point : previousPoint)
+        {
+            clearPoint(point.getKey(), point.getValue());
+        }
+        previousPoint.clear();
+    }
+
     public class Move implements MouseMotionListener
     {
 
         @Override
         public void mouseDragged(MouseEvent mouseEvent) {
-            if(choose ==1)
+            if(choose == Button.PENCIL)
             {
                 mX = mouseEvent.getX();
                 mY = mouseEvent.getY();
@@ -122,15 +154,22 @@ public class paint  extends JFrame {
 
         @Override
         public void mouseMoved(MouseEvent mouseEvent) {
-            if(choose == 2)
+            if(choose == Button.LINE)
             {
-                mX = mouseEvent.getX();
-                mY = mouseEvent.getY();
-                clearAll();
-                if(xStart != -1 && yStart !=-1)
-                 MidpointLine(xStart,yStart,mX,mY);
+                if (firstClick == true)
+                {
 
-                repaint();
+
+                }
+                else
+                {
+                    clearPrevious();
+                    mX = mouseEvent.getX();
+                    mY = mouseEvent.getY();
+                    if(xStart != -1 && yStart !=-1)
+                        MidpointLine(xStart,yStart,mX,mY);
+                    repaint();
+                }
             }
 
         }
@@ -141,8 +180,25 @@ public class paint  extends JFrame {
 
         @Override
         public void mouseClicked(MouseEvent mouseEvent) {
-            xStart = mouseEvent.getX();
-            yStart= mouseEvent.getY();
+            switch (choose)
+            {
+                case LINE:
+                {
+                    xStart = mouseEvent.getX();
+                    yStart= mouseEvent.getY();
+                    if (firstClick == true)
+                    {
+                        firstClick = false;
+                    }
+                    else
+                    {
+                        firstClick = true;
+                        previousPoint.clear();
+                    }
+                    break;
+                }
+            }
+
         }
 
         @Override
@@ -177,7 +233,7 @@ public class paint  extends JFrame {
             String nameButton = actionEvent.getActionCommand();
             if (nameButton.equals("clearButton"))
             {
-                choose =1;
+                choose = Button.CLEAR;
                 clearAll();
                 repaint();
             }
@@ -185,7 +241,9 @@ public class paint  extends JFrame {
             {
                 xStart=-1;
                 yStart=-1;
-                choose=2;
+                choose = Button.LINE;
+                firstClick = true;
+                previousPoint.clear();
             }
         }
     }
