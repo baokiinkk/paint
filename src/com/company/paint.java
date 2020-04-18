b3e13f52ec5519ae2ee37c66d7e10323db91e303
Tran Nguyen Chi Nhan <39859635+tncn1122@users.noreply.github.com>
2020-04-18 20:01:11 +0700

    add some fool things =))))

--- src/com/company/paint.java
+++ src/com/company/paint.java
@@ -27,6 +27,18 @@ public class paint  extends JFrame {
     private JButton clearButton;
     private JButton button5;
     private JButton button6;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+    private JComboBox comboBox1;
+    private JRadioButton radioButton1;
+    private JSpinner spinner1;
+    private JSlider slider1;
     private JTable table1;
 
     public paint() {
